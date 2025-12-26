28c4163b82bf164ef108d10a3376d0b7caf9078c
John Tsiombikas <nuclear@mutantstargoat.com>
2025-12-26 06:26:30 +0200

    implement freebsd load average

--- src/bsd/load.c
+++ src/bsd/load.c
@@ -1,11 +1,22 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/sysctl.h>
+#include <sys/resource.h>
 #include "xmon.h"
 
 int load_init(void)
 {
-	return -1;
+	return 0;
 }
 
 void load_update(void)
 {
+	struct loadavg la;
+	size_t len = sizeof la;
+
+	sysctlbyname("vm.loadavg", &la, &len, 0, 0);
+
+	smon.loadavg[0] = la.ldavg[0] / (float)la.fscale;
+	smon.loadavg[1] = la.ldavg[1] / (float)la.fscale;
+	smon.loadavg[2] = la.ldavg[2] / (float)la.fscale;
 }
